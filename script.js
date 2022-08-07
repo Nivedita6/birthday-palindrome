3eaf3f35c0e829e8dee730e77ad4990562fb7345
Nivedita AP <niveditaprasannaraj@gmail.com>
2022-08-07 20:23:50 +0530

    update: updated logic

--- script.js
+++ script.js
@@ -38,10 +38,129 @@ function convertDateToStr(date){
     return dateStr;
 }
 
-var date = {
-    day : 15,
-    month : 19,
-    year : 2022
+function getAllDateFormats(date){
+    var dateStr = convertDateToStr(date);
+
+    var ddmmyyyy =  dateStr.date + dateStr.month + dateStr.year;
+    var mmddyyyy = dateStr.month + dateStr.date + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.date;
+    var ddmmyy = dateStr.date + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy = dateStr.month + dateStr.date + dateStr.year.slice(-2); 
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.date;
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
 }
 
-console.log(convertDateToStr(date));
+function checkPalindromeForAllDateFormats(date){
+    var listOfPalindromes = getAllDateFormats(date);
+
+    var flag = false;
+    for(var i = 0; i < listOfPalindromes.length; i++){
+        if(checkPalindrome(listOfPalindromes[i])){
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+
+function isLeapYear(year){
+    if(year % 400 === 0){
+        return true;
+    }
+    if(year % 100 === 0){
+        return false;
+    }
+    if(year % 4 === 0){
+        return true;
+    }
+    return false;
+}
+
+function getNextDate(date){
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if(month === 2){
+        if(isLeapYear(year)){
+            if(day > 29){
+                day = 1;
+                month++;
+            }
+        }
+        else{
+            if(day > 28){
+                day = 1;
+                month++;
+            }
+        }
+    }
+    else{
+        if(day > daysInMonth[month - 1]){
+            day = 1;
+            month++;
+        }
+    }
+
+    if(month > 12){
+        month =1;
+        year++;
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    };
+}
+
+function getNextPalindromeDate(date){
+    var ctr = 0;
+    var nextDate = getNextDate(date);
+
+    while(1){ //infinite loop
+        ctr++;
+        var checkPalindrome = checkPalindromeForAllDateFormats(nextDate);
+        
+        if(checkPalindrome){
+            break;
+        }
+        nextDate = getNextDate(nextDate);
+    }
+
+    return [ctr, nextDate];
+}
+
+var dateInput = document.querySelector("#bday-input");
+var output = document.querySelector("#result");
+var showBtn = document.querySelector("#show-button");
+
+function clickHandler(){
+    var bdayStr = dateInput.value;
+
+    if(bdayStr !== ''){
+        var listOfDate = bdayStr.split('-');
+
+        var date = {
+            day: Number(listOfDate[2]),
+            month: Number(listOfDate[1]),
+            year: Number(listOfDate[0])
+        }; 
+
+        var isPalindrome = checkPalindromeForAllDateFormats(date);
+        if(isPalindrome){
+            output.innerText = "Yay! your birthday is a palindrome"
+        }
+        else{
+            var [ctr, nextDate] = getNextPalindromeDate(date);
+            output.innerText = "The next palindrome date is in " + nextDate.day + "-" + nextDate.month + "-" + nextDate.year + ". You missed it by  " + ctr + " days."
+        }
+
+    }
+}
+
+showBtn.addEventListener("click", clickHandler);
+
